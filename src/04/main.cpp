377c17092b6f90e65a41fd7439843fbe30441a7f
DNKpp <dnkpp2011@gmail.com>
2022-12-04 14:16:16 +0100

    add day 4 part 1 solution

---
+++ src/04/main.cpp
@@ -0,0 +1,67 @@
+#include <fstream>
+#include <filesystem>
+#include <ranges>
+#include <cassert>
+#include <regex>
+
+#include <fmt/format.h>
+#include <range/v3/all.hpp>
+#include <Simple-Utility/functional.hpp>
+
+#include "utility.hpp"
+
+// solves https://adventofcode.com/2022/day/4
+
+namespace sf = sl::functional;
+
+struct bounds
+{
+	int begin;
+	int end;
+
+	[[nodiscard]]
+	constexpr bool contains(const bounds& other) const noexcept
+	{
+		return begin <= other.begin && other.end <= end;
+	}
+};
+
+void do_part1()
+{
+	std::ifstream in{ std::filesystem::path{ INPUT_DIR } / "input.txt" };
+
+	const auto result = std::ranges::count_if(
+		getline_range{ in }
+		| std::views::transform(
+			[](const std::string_view str)
+			{
+				static const std::regex pattern{ R"((\d+)-(\d+),(\d+)-(\d+))" };
+				std::cmatch matchResult{};
+				std::regex_match(str.data(), matchResult, pattern);
+				auto matchedInts = matchResult
+									| std::views::transform(&std::csub_match::str)
+									| std::views::transform(to_int);
+				return std::tuple{ bounds{ matchedInts[1], matchedInts[2] }, bounds{ matchedInts[3], matchedInts[4] } };
+			}
+		),
+		sf::tuple::apply(
+			[](const bounds& lhs, const bounds& rhs) {  return lhs.contains(rhs) || rhs.contains(lhs); }
+		)
+	);
+
+	fmt::print("part 1 answer is: {}\n", result);
+}
+
+void do_part2()
+{
+	// fmt::print("part 2 answer is: {}\n", result);
+}
+
+int main()
+{
+	do_part1();
+
+	do_part2();
+
+	return 0;
+}
